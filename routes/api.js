408b4e16f365d095bfc629635c0c82915b7f635f
Minhaul Abedin <55100658+MIARD@users.noreply.github.com>
2020-12-13 03:29:54 +0600

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -18,13 +18,26 @@ module.exports = function (app) {
   app.route('/api/convert')
     .get(function (req, res){
       let input = req.query.input;
+  
       let initNum = convertHandler.getNum(input);
+      console.log('initNum: '+initNum);
       let initUnit = convertHandler.getUnit(input);
+      console.log('initUnit: '+initUnit);
       let returnNum = convertHandler.convert(initNum, initUnit);
+      console.log('returnNum'+returnNum);
       let returnUnit = convertHandler.getReturnUnit(initUnit);
+      console.log('returnUnit: '+returnUnit);
       let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      console.log(toString);
       
-      //res.json
+      if(initUnit=='l')
+      initUnit='L';
+      if(!initNum || !initUnit) 
+        res.send(toString);
+      else{
+      let sendJson = {initNum:initNum,initUnit:initUnit,returnNum:returnNum,returnUnit:returnUnit,"string":toString}
+        res.json(sendJson)
+      }
     });
     
 };
